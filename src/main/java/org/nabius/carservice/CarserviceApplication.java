997427894fdb4f9fe36a0a000e810f5ca8bdeb39
NABiusster <nabius@ukr.net>
2024-10-03 18:11:45 +0200

    Initial commit

---
+++ src/main/java/org/nabius/carservice/CarserviceApplication.java
@@ -0,0 +1,13 @@
+package org.nabius.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarserviceApplication.class, args);
+	}
+
+}
